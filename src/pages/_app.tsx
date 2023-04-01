bcb408af2558adc08d4761b0bbfb805ae70f5eaa
TAZER <ajcoolx619@gmail.com>
2023-04-02 00:15:18 +0530

    Added Metamask Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,27 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import { createClient, configureChains, defaultChains, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
+
+const { provider, webSocketProvider } = configureChains(defaultChains, [publicProvider()]);
+
+const client = createClient({
+    provider,
+    webSocketProvider,
+    autoConnect: true,
+});
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <WagmiConfig client={client}>
+            <SessionProvider session={pageProps.session} refetchInterval={0}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </WagmiConfig>
+    );
+}
+
 
 export default api.withTRPC(MyApp);
