36a1a852cf10110d0d402b4bcbbc4da4dc569678
IsomerX <49413460+IsomerX@users.noreply.github.com>
2023-04-02 00:53:56 +0530

    asd

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,15 @@
+import { useSession } from "next-auth/react";
+
 /* eslint-disable @next/next/no-img-element */
 const Login = () => {
+  const { status } = useSession();
+  console.log(status);
   return (
     <div className="flex h-screen overflow-hidden bg-primary">
       <img
         src="/images/login-banner.png"
         alt="banner"
-        className="h-screen w-1/2 object-cover "
+        className="h-screen w-1/2 object-cover"
       />
     </div>
   );
